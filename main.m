d50f86ba43fc5050570af536fca263de1bc9d1fd
mayankdubey0 <108892930+mayankdubey0@users.noreply.github.com>
2024-01-18 18:46:36 -0600

    Update main.m

--- main.m
+++ main.m
@@ -1,10 +1,9 @@
 drone = Drone();
 drone_cont = Drone_Controller(drone);
-drone_cont.drone.show_drone();
-numSteps = 10;
+drone_cont.drone.set_grid();
+numSteps = 200;
 for step = 1:numSteps
     drone_cont.drone.show_drone();
-    drone_cont.find_current_state();
+    drone_cont.update_current_state();
 end
 drone.show_drone();
-drone_cont.find_current_state();
