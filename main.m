df202d2c16076b33be53ec54cd827c92b5bf7d19
mayankdubey0 <108892930+mayankdubey0@users.noreply.github.com>
2024-01-21 23:21:35 -0600

    Update main.m

--- main.m
+++ main.m
@@ -4,6 +4,7 @@ drone_cont.drone.set_grid();
 numSteps = 200;
 for step = 1:numSteps
     drone_cont.drone.show_drone();
-    drone_cont.update_current_state();
+    %drone_cont.update_current_state();
+    drone_cont.LQR();
 end
 drone.show_drone();
