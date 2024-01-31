9b5c0128e7cbc98f8a7d8ff49816d874e6e3a210
mayankdubey0 <108892930+mayankdubey0@users.noreply.github.com>
2024-01-30 22:52:00 -0600

    Update main.m

--- main.m
+++ main.m
@@ -1,10 +1,20 @@
 drone = Drone();
 drone_cont = Drone_Controller(drone);
 drone_cont.drone.set_grid();
-numSteps = 200;
+up = [0, 0, 1];
+n = cross(drone.m1, drone.m4)/norm(cross(drone.m1, drone.m4));
+%disp(n);
+%disp(asin(cross(n, up)));
+numSteps = 100;
 for step = 1:numSteps
     drone_cont.drone.show_drone();
-    %drone_cont.update_current_state();
-    drone_cont.LQR();
+    drone_cont.update_current_state();
+    disp('Error');
+    drone_cont.control();
+    % n = cross(drone.m1, drone.m4)/norm(cross(drone.m1, drone.m4));
+    % disp('n');
+    % disp(n);
+    % disp('true angle');
+    % disp(asin(cross(n, up)));
 end
-drone.show_drone();
+disp("#######################");
